70ef668e5cd4c582903b95a448bf9d829c9bad26
yuljul <yulduz@gmail.com>
2022-12-29 15:17:52 +0500

    first save

---
+++ main.js
@@ -0,0 +1,58 @@
+
+
+var myname = prompt('Введите свое имя :)');
+
+console.log(myname);
+
+
+var myage = +prompt('Введите свой возвраст');
+
+console.log(myage);
+
+
+
+
+var addnumber = +prompt('Решите пример 6+3 =');
+
+console.log('Верный ответ =9' + ' Ваш ответ ' + (addnumber))
+
+var dividenumber = +prompt('Решите пример 6/3 =');
+
+console.log('Верный ответ =2' + ' Ваш ответ ' + (dividenumber))
+
+var minusnumber = +prompt('Решите пример 6-3 =');
+
+console.log('Вверный ответ =3' + ' Ваш ответ ' + (minusnumber))
+
+var multinumber = +prompt('Решите пример 6*3 =');
+
+console.log('Верный ответ =18' + ' Ваш ответ ' + (multinumber))
+
+var multinumber = +prompt('Решите пример 6%3 =');
+
+console.log('Верный ответ =0' + ' Ваш ответ ' + (multinumber))
+
+
+
+
+
+var firstnumber = +prompt('Введите первое число от 1 до 100');
+
+alert(firstnumber);
+
+var secondnumber = +prompt('Введите второе число от 1 до 100');
+
+alert(secondnumber);
+
+var thirdnumber = +prompt('Введите третье число от 1 до 100');
+
+alert(thirdnumber);
+
+
+
+alert ( 'Среднее значения трех перемен ' + ((firstnumber) + (secondnumber) + (thirdnumber))  / 3 );
+
+
+
+
+
